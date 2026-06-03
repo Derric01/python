b025282f6b3b11fc214fd3c508e4463004a027c5
Derric samson <140136414+Derric01@users.noreply.github.com>
2026-06-04 00:31:16 +0530

    twosum problem from leet code

---
+++ list_based_problems/twosum.py
@@ -0,0 +1,13 @@
+target=int(input())
+nums=list(map(int, input().split()))
+class solution:
+           def twosum(self,nums,target):
+            n=len(nums)
+            for i in range(n):
+               for j in range(i+1,n):
+                   if nums[i]+nums[j]==target:
+                        return [i,j] 
+
+sol = solution()
+print(sol.twosum(nums,target))
+
